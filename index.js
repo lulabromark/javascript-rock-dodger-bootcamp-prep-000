6f2c81dafc75f6aab34983e2d70c3b5a0ad2f738
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-09 04:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -117,7 +117,7 @@ function createRock(x) {
 function endGame() {
   clearInterval(gameInterval);
   for (let i = 0; i < ROCKS.length; i++) {
-    GAME.removeChild.(ROCKS[i])
+    ROCKS[i].remove()
   }
   window.removeEventListener('keydown', moveDodger)
 }
