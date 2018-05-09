e1da87aefede31612054dcb663497cca3206793a
Mark Lawrence <lawrence.ml@gmail.com>
2018-05-09 03:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -78,8 +78,8 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
-     if (checkCollision === true) {
-       endGame
+     if (checkCollision = true) {
+       endGame()
      }
 
     /**
